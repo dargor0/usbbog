c60c9521f688e8f3716118116a4bcffadfc51810
Oscar Diaz <odiaz@ieee.org>
2025-02-25 20:20:09 -0500

    edit - POO - updated template for PEP8

--- poo_2025_1/template.py
+++ poo_2025_1/template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Título de práctica: breve descripción 
+Título de práctica: breve descripción
 
 Descripción extendida del programa
 
@@ -9,19 +9,21 @@ Autor: ElAutor <el@correo>
 Fecha: 2025-02-01
 """
 
-### En esta región puede importar los módulos necesarios para su programa
-### O las definiciones de clases y/o funciones que requiera.
+# **** En esta región puede importar los módulos necesarios para su programa
+# **** O las definiciones de clases y/o funciones que requiera.
+
 
 def run():
     """script entrypoint"""
 
-    ### Poner el código ejecutable de su ejercicio aquí
+    # **** Poner el código ejecutable de su ejercicio aquí
 
     # Saludo
     print("Hola mundo!")
 
-    ### ****
+    # **** ****
 
-### Conserve este condicional para ejecutar el programa directamente
+
+# **** Conserve este condicional para ejecutar el programa directamente
 if __name__ == "__main__":
     run()
