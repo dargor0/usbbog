b6e311f796dd937f046af290ddd73722135feac1
Oscar Diaz <odiaz@ieee.org>
2026-09-22 17:05:02 -0500

    Digitales - seven segment decoder example for nexys4ddr without decoder table

---
+++ digitales_2026_2/sevensegdec.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Seven segment decoder: Example script to generate a seven segment decoder
+based on a lookup table (LUT). It includes testing and conversion to VHDL/verilog
+"""
+
+from myhdl import *
+import argparse
+
+# implementation
+
+@block
+def sevensegdec(bcd: Signal, sseg: Signal, decod_table: tuple, invert: bool = False):
+    """Seven segment decoder"""
+
+    # internal signal for optional bit inversion
+    ssegout = Signal(intbv(0)[7:])
+
+    @always_comb
+    def logic():
+        """LUT implementation: based on a decoder table"""
+        ssegout.next = decod_table[int(bcd)]
+
+    @always_comb
+    def invertproc():
+        """Inverting process for output"""
+        if invert:
+            sseg.next = ~ssegout
+        else:
+            sseg.next = ssegout
+        
+    # this will include logic and invertproc
+    return instances()
+    
+# testbench
+
+@block
+def sevensegdec_tb(decod_table: tuple, invert: bool = False):
+    """Testbench for Seven segment decoder"""
+
+    # Testbench signals
+    bcd_tb = Signal(intbv(0)[4:])
+    sseg_tb = Signal(intbv(0)[7:])
+    
+    uut = sevensegdec(bcd_tb, sseg_tb, decod_table, invert)
+    
+    @instance
+    def stimulus():
+        """Stimulus generation"""
+
+        # run for each possible value
+        for bcd_in in range(len(decod_table)):
+            bcd_tb.next = bcd_in
+            yield delay(10)
+            
+    @always_comb
+    def check():
+        """Verification"""
+
+        # It needs to discard first iteration
+        if now() > 0:
+            sseg_expect = decod_table[int(bcd_tb)]
+            if sseg_tb.val != sseg_expect:
+                print(f"ERROR: on ({now()}) bcd input '{bcd_tb.val}': expected differ {sseg_expect} != {sseg_tb}")
+            
+    # this will include uut, stimulus and check
+    return instances()
+
+@block
+def sevensegdec_nexys(bcd: Signal, sseg: Signal, bcdled: Signal, ssanodes: Signal, decod_table: tuple, invert: bool = False):
+    """Envelope for running a demo in the Nexys4 DDR board for the Seven segment decoder"""
+
+    # Main decoder instance
+    ssdec = sevensegdec(bcd, sseg, decod_table, invert)
+
+    @always_comb
+    def logic():
+        """Process to fill the LEDs for switches and display anodes"""
+        bcdled.next = bcd
+        ssanodes.next = intbv(0xfe)[8:] # only turn on first display, it is turned on with 0.
+
+    # this will include ssdec and logic
+    return instances()
+
+def read_table(table_file: str) -> tuple:
+    """Read the decoder table from text file:
+        * Each line contains a string with 0s and 1s, in order "abcdefg"
+        * It must contain 16 lines, one for each 4-bit digit
+    """
+    if table_file != "":
+        try:
+            with open(table_file) as f:
+                return tuple([int(x, base=2) for x in f.readlines()])
+        except Exception as ex:
+            print(f"Exception reading the table: {ex}")
+    return tuple([0 for x in range(16)])
+    
+def run():
+    """Entrypoint"""
+    parser = argparse.ArgumentParser(description="Seven segment decoder")
+    parser.add_argument(
+        "--simulation",
+        action="store_true",
+        help="If enabled, run simulation in python",
+    )
+    parser.add_argument(
+        "--invert",
+        action="store_true",
+        help="Invert the outputs (simulation only)",
+    )
+    parser.add_argument(
+        "--verilog",
+        action="store_true",
+        help="Convert to verilog instead of VHDL",
+    )
+    parser.add_argument(
+        "--table",
+        type=str,
+        default="",
+        help="Decoder table to include")
+    args = parser.parse_args()
+
+    if args.table != "":
+        table = read_table(args.table)
+    else:
+        print(f"No table entered, put zero table by default.")
+        table = read_table("")
+
+    if args.simulation:
+        # simulation
+        tb = sevensegdec_tb(args.invert, table)
+        tb.config_sim(trace=True)
+        tb.run_sim()
+        print("Simulation done.")
+    
+    # conversion
+
+    # Signal list for sevensegdec_nexys
+    ss_sig = {
+        "bcd": Signal(intbv(0)[4:]),
+        "sseg": Signal(intbv(0)[7:]),
+        "bcdled": Signal(intbv(0)[4:]),
+        "ssanodes": Signal(intbv(0)[8:]),
+        "invert": Signal(False),
+        "decod_table": table
+        }
+    sscomponent = sevensegdec_nexys(**ss_sig)
+
+    # which language output
+    if args.verilog:
+        langout = "verilog"
+    else:
+        langout = "VHDL"
+    sscomponent.convert(hdl=langout)
+
+    print(f"Conversion done ({langout}).")
+    
+if __name__ == "__main__":
+    run()
